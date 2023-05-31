226e59aaa22448b3429406633527aea140eb5f4d
William Thorsen <william@thorsen.dev>
2023-05-30 23:38:40 -0700

    fix|strings: Fix incompatible types paths

--- libraries/strings/scripts/build-npm.ts
+++ libraries/strings/scripts/build-npm.ts
@@ -44,11 +44,6 @@ await build({
       url: "git+https://github.com/williamthorsen/toolbelt.git",
     },
     license: "UNLICENSED",
-    typesVersions: {
-      "*": {
-        "*": ["./types"],
-      },
-    },
     publishConfig: {
       access: "public",
       registry: "https://npm.pkg.github.com",
