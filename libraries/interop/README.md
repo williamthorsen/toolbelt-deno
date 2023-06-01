33fc728645eedfa7dee03af47d9f82b924b9680a
William Thorsen <william@thorsen.dev>
2023-05-31 21:19:34 -0700

    util|bundle: Add reusable function to create Node module
    Added the `buildNodeModule` function

---
+++ libraries/interop/README.md
@@ -0,0 +1,3 @@
+## Deno-Node utilities
+
+This directory contains utilities for handling interop between Deno and Node.
---
+++ libraries/tooling/isValidSemver.ts
@@ -0,0 +1,7 @@
+/**
+ * Returns true if the string is a valid semver string.
+ * Prerelease versions such as "1.0.0-alpha.1" are allowed.
+ */
+export function isValidSemver(version: string): boolean {
+  return /^\d+\.\d+\.\d+(-.+)?$/.test(version);
+}
