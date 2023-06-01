02178acd6ff9b273dd4eaba4faf8a8fb1243ca03
William Thorsen <william@thorsen.dev>
2023-05-31 21:27:32 -0700

    fmt|strings: Reformat code

--- libraries/strings/src/__tests__/test.deps.ts
+++ libraries/strings/src/__tests__/test.deps.ts
@@ -1,5 +1,2 @@
-export {
-  assertEquals,
-  assertThrows,
-} from 'https://deno.land/std@0.190.0/testing/asserts.ts';
+export { assertEquals, assertThrows } from 'https://deno.land/std@0.190.0/testing/asserts.ts';
 export { describe, it } from 'https://deno.land/std@0.190.0/testing/bdd.ts';
