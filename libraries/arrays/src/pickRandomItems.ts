61703941b764a36a132cbee049bf62b155d8786d
William Thorsen <william@thorsen.dev>
2023-06-16 06:22:39 -0700

    feat|arrays: Can skip items when picking random items
    `pickRandomItems` now accepts an `offset` option for use with a deterministic seed

--- libraries/arrays/src/pickRandomItems.ts
+++ libraries/arrays/src/pickRandomItems.ts
@@ -3,16 +3,23 @@ import { shuffle } from './shuffle.ts';
 /**
  * Returns an array with `n` items picked randomly from the given array.
  * Optionally accepts a seed that makes the results deterministic.
+ * Optionally accepts an offset that causes the first `offset` items to be omitted from the result
+ * (intended for use with a seed, but it does no harm if used alone).
  * TODO: Optionally throw an error if `n` is greater than the length of the array.
  */
 export function pickRandomItems<T>(items: ReadonlyArray<T>, n: number, options: Options = {}): T[] {
-  const { seed } = options;
+  const { offset, seed } = options;
 
   const tempItems = shuffle(items, { seed });
+
+  if (offset) {
+    return tempItems.slice(offset, n);
+  }
 
   return tempItems.slice(0, n);
 }
 
-interface Options {
+type Options = {
+  offset?: number | undefined;
   seed?: number | undefined;
-}
+};
