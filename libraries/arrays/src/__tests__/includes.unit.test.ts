0199ba287a34883ce668b76622bd03c5af9e6538
William Thorsen <william@thorsen.dev>
2023-07-16 17:30:35 -0700

    arrays|feat: Add type-safe replacement for Array#includes
    Added the `includes` function

---
+++ libraries/arrays/src/__tests__/includes.unit.test.ts
@@ -0,0 +1,35 @@
+import { assertEquals, describe, it } from '../../dev_deps.ts';
+
+import { includes } from '../includes.ts'; // Your includes function import path
+
+describe('includes()', () => {
+  it('returns true if the array includes the item', () => {
+    const arr = [1, 2, 3];
+
+    assertEquals(includes(arr, 2), true);
+  });
+
+  it('returns false if the array does not include the item', () => {
+    const arr = [1, 2, 3];
+
+    assertEquals(includes(arr, 4), false);
+  });
+
+  it('works correctly with readonly arrays', () => {
+    const arr = ['a', 'b', 'c'] as const;
+
+    assertEquals(includes(arr, 'b'), true);
+    assertEquals(includes(arr, 'd'), false);
+  });
+
+  it('works correctly with array of different types', () => {
+    const arr = [1, 'a', true, null, undefined] as const;
+
+    assertEquals(includes(arr, 1), true);
+    assertEquals(includes(arr, 'a'), true);
+    assertEquals(includes(arr, true), true);
+    assertEquals(includes(arr, null), true);
+    assertEquals(includes(arr, undefined), true);
+    assertEquals(includes(arr, 'b'), false);
+  });
+});
