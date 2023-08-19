550ceb686c5e2327fb2b109b8174bb85ed114b0a
William Thorsen <william@thorsen.dev>
2023-08-19 01:06:05 -0700

    arrays|fix: Type exports from weighting entrypoint are not resolved
    The separate `weighting` entrypoint is not working in calling code. Switched to a single entrypoint as a workaround.

--- libraries/arrays/weighting.ts
+++
@@ -1,5 +0,0 @@
-export { getCumulativeWeights } from './src/getCumulativeWeights.ts';
-export { getWeightedIndex } from './src/getWeightedIndex.ts';
-export { pickWeightedIndex } from './src/pickWeightedIndex.ts';
-export { toPickWeightedItem } from './src/toPickWeightedItem.ts';
-export { toPickWeightedItemFromDistribution } from './src/toPickWeightedItemFromDistribution.ts';
