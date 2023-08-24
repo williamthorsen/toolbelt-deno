f126d06a2034ab33ff298cccd0fd5601bda65e4b
William Thorsen <william@thorsen.dev>
2023-08-23 23:48:02 -0700

    stats|feat: Export new functions for use

--- libraries/stats/mod.ts
+++ libraries/stats/mod.ts
@@ -1,4 +1,6 @@
 export { computeCdf } from './src/computeCdf.ts';
+export { computeCdfInverse } from './src/computeCdfInverse.ts';
+export { findDistributionByIntervalProbability } from './src/findDistributionByIntervalProbability.ts';
 export { getNormalIntervalProbabilities } from './src/getNormalIntervalProbabilities.ts';
 export { toAddendsFromCumulativeSums } from './src/toAddendsFromCumulativeSums.ts';
 export { toCumulativeSumsFromAddends } from './src/toCumulativeSumsFromAddends.ts';
