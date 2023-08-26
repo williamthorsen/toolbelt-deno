b26af3476368a9f512b4a565e1dc2df51c3ec912
William Thorsen <william@thorsen.dev>
2023-08-26 14:32:40 -0700

    root|tooling: Add scripts to build all libraries & lock files

---
+++ scripts/update-deps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Only libraries with a build step need to have their dependencies updated.
+local LIBRARIES=("arrays" "dstructs" "numbers" "objects" "strings")
+
+rm -f deno.lock
+deno cache --lock=deno.lock --lock-write --unstable dev_deps.ts
+
+for library in "${LIBRARIES[@]}"; do
+  cd libraries/$library
+  rm -f deno.lock
+  deno task check && deno task build
+  cd ../..
+done
