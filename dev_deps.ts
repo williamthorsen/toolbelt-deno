3d3b267c75826688eaa00e5868bc35c1f6d7007d
William Thorsen <william@thorsen.dev>
2023-08-27 08:12:36 -0700

    *|deps: Use updated source for asserts

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,11 +1,9 @@
-export {
-  assert,
-  assertAlmostEquals,
-  assertArrayIncludes,
-  assertEquals,
-  assertMatch,
-  assertNotEquals,
-  assertNotStrictEquals,
-  assertThrows,
-} from 'https://deno.land/std@0.200.0/testing/asserts.ts';
+export { assert } from 'https://deno.land/std@0.200.0/assert/assert.ts';
+export { assertAlmostEquals } from 'https://deno.land/std@0.200.0/assert/assert_almost_equals.ts';
+export { assertArrayIncludes } from 'https://deno.land/std@0.200.0/assert/assert_array_includes.ts';
+export { assertEquals } from 'https://deno.land/std@0.200.0/assert/assert_equals.ts';
+export { assertMatch } from 'https://deno.land/std@0.200.0/assert/assert_match.ts';
+export { assertNotEquals } from 'https://deno.land/std@0.200.0/assert/assert_not_equals.ts';
+export { assertNotStrictEquals } from 'https://deno.land/std@0.200.0/assert/assert_not_strict_equals.ts';
+export { assertThrows } from 'https://deno.land/std@0.200.0/assert/assert_throws.ts';
 export { afterEach, beforeEach, describe, it } from 'https://deno.land/std@0.200.0/testing/bdd.ts';
