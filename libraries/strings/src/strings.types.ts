d148a235a94b5d54253174126c7522db5427c58a
William Thorsen <william@thorsen.dev>
2023-09-14 16:01:24 -0700

    strings|refactor: Simplify API of interpolate
    Replaced overloads and kludgy function signature with a correct signature

--- libraries/strings/src/strings.types.ts
+++ libraries/strings/src/strings.types.ts
@@ -1,3 +1,9 @@
+export type StringIndexedMapping = Map<string, string> | Record<string, string> | string[];
+
+export type StringInterfaceMapping<O> = O extends { [K in keyof O]: string } ? O : never;
+
+export type StringMapping<T> = StringIndexedMapping | StringInterfaceMapping<T>;
+
 // Ignore the warning about `Function`: We do want to exclude all functions and classes!
 // deno-lint-ignore ban-types
 export type StrictObject<T> = T extends (null | Function | ArrayLike<unknown>) ? never : (T extends object ? T : never);
