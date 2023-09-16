cbeeaa0928c75f78918b3413d0b5dea91dfd2510
William Thorsen <william@thorsen.dev>
2023-09-16 13:47:09 -0700

    strings|feat: Can convert a string to camel case
    Added function: `toCamelCase`

--- libraries/strings/mod.ts
+++ libraries/strings/mod.ts
@@ -2,5 +2,6 @@ export { Interpolable } from './src/Interpolable.ts';
 export { interpolate } from './src/interpolate.ts';
 export { pickVariants } from './src/pickVariants.ts';
 export { slugify } from './src/slugify.ts';
+export { toCamelCase } from './src/toCamelCase.ts';
 export { unindent } from './src/unindent.ts';
 export { validateDelimiters } from './src/validateDelimiters.ts';
---
+++ libraries/strings/src/toCamelCase.ts
@@ -0,0 +1,23 @@
+/**
+ * Returns the camelCase version of the input string.
+ * Spaces, hyphens, and underscores are treated as word boundaries.
+ * @param inputStr - The string to be converted.
+ * @returns The input string converted to camelCase.
+ */
+export function toCamelCase(inputStr: string): string {
+  // Trim and split the input string into substrings on whitespace and separators
+  const words = inputStr.trim().split(/[\s-_]+/);
+
+  // Convert all substrings to lowercase, then capitalize every word after the first
+  return words
+    .map((word, index) => {
+      const lowerCaseWord = word.toLowerCase();
+
+      // Return the first word as is; capitalize every word thereafter
+      return index === 0 ? lowerCaseWord : capitalizeFirstLetter(lowerCaseWord);
+    })
+    .join('');
+}
+function capitalizeFirstLetter(input: string): string {
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
