7bc9c0d1a75409a76e12da22e096d72672c2fff3
William Thorsen <william@thorsen.dev>
2023-09-20 00:41:40 -0700

    numbers|fix: Type issues prevent use of subclasses
    Replaced subclasses with instance presets.

--- libraries/numbers/src/Seed.ts
+++ libraries/numbers/src/Seed.ts
@@ -6,11 +6,16 @@ import { pickInteger } from './pickInteger.ts';
  * Number-generating class for use with functions that accept a `Seed` or `SeedLike` parameter.
  */
 export class Seed {
-  protected get maxBase() {
-    return Number.MAX_SAFE_INTEGER;
-  }
+  private _base: number; // internally incremented value to provide deterministic behaviour
+  readonly preset: 'int' | 'int32' | 'standard' = 'standard';
+  private readonly maxBase = Number.MAX_SAFE_INTEGER;
 
-  protected _base: number; // internally incremented value to provide deterministic behaviour
+  private readonly generateBase: () => number;
+  private readonly generateValue: () => number;
+
+  static create(seed?: SeedLike, options: SeedOptions = {}): Seed {
+    return new Seed(seed, options);
+  }
 
   static evaluate(seed: SeedLike | undefined): number | undefined {
     if (typeof seed === 'number') {
@@ -25,19 +30,23 @@ export class Seed {
   // Creates a child; does not mutate the parent seed
   // By default, the child's base is incremented by 1 to produce a different output than the parent
   // while still being deterministic. Set nIncrements to 0 to produce the same output.
-  static clone<T extends ThisConstructor<typeof Seed>>(
-    this: T,
-    seed: SeedLike | undefined,
-    nIncrements = 1,
-  ): This<T> | undefined {
-    if (seed instanceof this) {
-      return this.spawn(seed.base)?.increment(nIncrements);
+  static clone(seed?: undefined, nIncrements?: number): undefined;
+  static clone(seed: SeedLike, nIncrements?: number): Seed;
+  static clone(seed: SeedLike | undefined, nIncrements = 1): Seed | undefined {
+    if (seed instanceof Seed) {
+      return Seed.create(seed.base, { preset: seed.preset }).increment(nIncrements);
     }
   }
 
   // Creates a child; mutates the input seed, if it is a Seed instance or generator
-  static spawn<T extends ThisConstructor<typeof Seed>>(this: T, seed: SeedLike | undefined): This<T> | undefined {
-    return seed === undefined ? undefined : new this(seed);
+  static spawn(seed?: undefined): undefined;
+  static spawn(seed: SeedLike): Seed;
+  static spawn(seed?: SeedLike): Seed | undefined {
+    if (seed === undefined) {
+      return undefined;
+    }
+    const preset = seed instanceof Seed ? seed.preset : undefined;
+    return new Seed(seed, { preset });
   }
 
   /**
@@ -57,14 +66,27 @@ export class Seed {
     fn: (options?: OptionsWithSeed<TOptions> | OptionsWithSeed<EmptyObject>) => R,
     seed: SeedLike | undefined,
   ) {
-    const spawnedSeed = this.spawn(seed); // stored outside the function to create a closure
+    const spawnedSeed = Seed.spawn(seed); // stored outside the function to create a closure
     return function (options?: TOptions) {
       return fn({ ...options, seed: spawnedSeed });
     };
   }
 
-  constructor(seed?: SeedLike) {
-    this._base = ((new.target).evaluate(seed) ?? this.generateBase()) % this.maxBase;
+  constructor(seed?: SeedLike, options: SeedOptions = {}) {
+    const { preset = 'standard' } = options;
+    if (preset === 'int') {
+      this.generateBase = () => pickInteger({ min: 1 });
+      this.generateValue = () => pickInteger({ min: 0, seed: getFakeMathRandom(this.nextBase()) });
+    } else if (preset === 'int32') {
+      this.generateBase = () => pickInteger({ min: 1, max: this.maxBase });
+      this.generateValue = () => pickInteger({ min: 0, max: this.maxBase, seed: getFakeMathRandom(this.nextBase()) });
+      this.maxBase = 4294967295; // 2^32 - 1
+    } else {
+      this.generateBase = () => Math.random();
+      this.generateValue = () => getFakeMathRandom(this.nextBase());
+    }
+    this.preset = preset;
+    this._base = (Seed.evaluate(seed) ?? this.generateBase()) % this.maxBase;
   }
 
   get base(): number {
@@ -76,8 +98,8 @@ export class Seed {
     return () => this.generateValue();
   }
 
-  clone<T extends Seed>(this: T, nIncrements = 0): T {
-    return new (this.constructor as Constructor<T>)(this._base).increment(nIncrements);
+  clone(nIncrements = 1): Seed {
+    return Seed.clone(this, nIncrements);
   }
 
   increment<T extends Seed>(this: T, n: number): T {
@@ -89,14 +111,6 @@ export class Seed {
     return this.generateValue();
   }
 
-  protected generateBase() {
-    return Math.random();
-  }
-
-  protected generateValue() {
-    return getFakeMathRandom(this.nextBase());
-  }
-
   // Returns the base value and then safely increments it (safe equivalent to _base++)
   protected nextBase(): number {
     const base = this._base;
@@ -105,42 +119,21 @@ export class Seed {
   }
 }
 
-export class IntSeed extends Seed {
-  protected generateBase() {
-    return pickInteger({ min: 1 });
-  }
-
-  protected generateValue() {
-    return pickInteger({ min: 0, seed: getFakeMathRandom(this.nextBase()) });
-  }
-}
-
-export class Int32Seed extends Seed {
-  protected get maxBase() {
-    return 4294967295; // 2^32 - 1
-  }
-
-  protected generateBase() {
-    return pickInteger({ min: 1, max: this.maxBase });
-  }
-
-  protected generateValue() {
-    return pickInteger({ min: 0, seed: getFakeMathRandom(this.nextBase()) });
-  }
-}
-
 // region | Types
-// deno-lint-ignore no-explicit-any
-type Constructor<T, Arguments extends unknown[] = any[]> = new (...arguments_: Arguments) => T;
-
 type OptionsWithSeed<O> = O & { seed?: SeedLike };
 
 type SeedFunction = () => number;
 
 export type SeedLike = number | Seed | SeedFunction | undefined;
 
-type ThisConstructor<
-  T extends { prototype: unknown } = { prototype: unknown },
-> = T;
-type This<T extends ThisConstructor> = T['prototype'];
+interface SeedOptions {
+  preset?: SeedPreset | undefined;
+}
+
+type SeedPreset = 'int' | 'int32' | 'standard';
+
+interface SpawnOptions {
+  preset?: SeedPreset | undefined;
+}
+
 // endregion | Types
