354210167971ea294a6d8b7d6cb2421212fde5ac
William Thorsen <william@thorsen.dev>
2023-10-04 12:55:43 -0700

    objects|format: Simplify code
    Simplified the syntax of `hasOwnProperty`

--- libraries/objects/src/hasOwnProperty.ts
+++ libraries/objects/src/hasOwnProperty.ts
@@ -5,13 +5,8 @@ export function hasOwnProperty<T, K extends PropertyKey>(
   target: T,
   key: K,
 ): target is T & Record<K, K extends keyof T ? T[K] : never> {
-  if (target === null) {
-    return false;
-  }
-
-  if (!['object', 'function'].includes(typeof target)) {
-    return false;
-  }
+  if (target === null) return false;
+  if (!['object', 'function'].includes(typeof target)) return false;
 
   return Object.prototype.hasOwnProperty.call(target, key);
 }
