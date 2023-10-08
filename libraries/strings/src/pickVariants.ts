3d002dc0e7dda86d34e8ba06f782776eed7986ee
William Thorsen <william@thorsen.dev>
2023-10-07 21:39:58 -0700

    strings|refactor: Use dedicated function to validate delmiiters
    Replaced the inline delimiter-validation code in `pickVariants` with call to `validateDelimiters`

--- libraries/strings/src/pickVariants.ts
+++ libraries/strings/src/pickVariants.ts
@@ -1,5 +1,6 @@
 import type { Seed } from '../sibling_deps.ts';
 import { pickItem, SeededRng } from '../sibling_deps.ts';
+import { validateDelimiters } from './validateDelimiters.ts';
 
 /**
  * Given a string that contains delimited variants [variant1|variant2|variant3], replaces the variants with a
@@ -11,18 +12,9 @@ export function pickVariants(text: string, options: Options = {}): string {
   const seed = SeededRng.spawn(options.seed);
 
   // Check that the delimiters are correctly nested
-  const stack = [];
-  for (const char of text) {
-    if (char === '[') {
-      stack.push(char);
-    } else if (char === ']') {
-      if (stack.length === 0 || stack.pop() !== '[') {
-        throw new Error('Variant delimiters [ ] are incorrectly nested.');
-      }
-    }
-  }
-  if (stack.length > 0) {
-    throw new Error('Variant delimiters [ ] are incorrectly nested.');
+  const validationResult = validateDelimiters(text, { opening: '[', closing: ']' });
+  if (!validationResult.isValid) {
+    throw new Error(validationResult.errors[0].message);
   }
 
   const variantRegex = /\[([^\[\]]*?)]/g;
