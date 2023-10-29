8effe9ad6c07b7ce4ccd520d6be0321ce432b3e8
William Thorsen <william@thorsen.dev>
2023-10-29 05:07:34 -0700

    objects|feat: Can provide a default value for the getter function
    Modified `get` to accept an optional default value

--- libraries/objects/src/__tests__/get.unit.test.ts
+++ libraries/objects/src/__tests__/get.unit.test.ts
@@ -13,6 +13,16 @@ describe('get()', () => {
     assertEquals(actual, expected);
   });
 
+  it('narrows the type to exclude undefined when undefined is not allowed', () => {
+    const key = 'items';
+    const obj: { items: string[] } = { items: ['apple'] };
+
+    const value = get(key)(obj);
+
+    // `value.length` raises no type error because the return type is correctly constrained to `string[]`.
+    assertEquals(value.length, 1);
+  });
+
   it('returns undefined for a nonexistent key', () => {
     const key = 'b';
     const obj: { a: string; b?: string } = { a: 'apple' };
@@ -31,5 +41,46 @@ describe('get()', () => {
     const actual = get(key)(obj);
 
     assertEquals(actual, expected);
+  });
+
+  describe('with default value', () => {
+    const defaultValue = 'avocado';
+    const getA = get('a', defaultValue);
+
+    it('if the property has a value, uses the value', () => {
+      const obj: { a?: string | undefined; b: string } = { a: 'apple', b: 'banana' };
+      const expected = 'apple';
+
+      const actual = getA(obj);
+
+      assertEquals(actual, expected);
+    });
+
+    it('if the property does not exist, uses the default value', () => {
+      const obj: { a?: string; b: string } = { b: 'banana' };
+      const expected = defaultValue;
+
+      const actual = getA(obj);
+
+      assertEquals(actual, expected);
+    });
+
+    it('if the property is undefined, uses the default value', () => {
+      const obj: { a?: string | undefined; b: string } = { a: undefined, b: 'banana' };
+      const expected = defaultValue;
+
+      const actual = getA(obj);
+
+      assertEquals(actual, expected);
+    });
+
+    it('the default value must be of the same type as the property', () => {
+      const obj: { a: string | undefined } = { a: 'apple' };
+
+      // @ts-expect-error Default value of `undefined` is not assignable to property of type 'string'.
+      const actual = get('a', undefined)(obj);
+
+      assertEquals(actual, 'apple');
+    });
   });
 });
--- libraries/objects/src/get.ts
+++ libraries/objects/src/get.ts
@@ -1,10 +1,20 @@
 /**
  * Returns a function that returns the value mapped to a key.
+ * If a default value is provided, the returned function will return the default value if the key is undefined.
  */
-export function get<TKey extends string>(prop: TKey) {
-  return function <TObj extends Partial<Record<TKey, unknown>>>(
-    value: TObj,
-  ): TObj[TKey] {
-    return value[prop];
+export function get<TKey extends string>(
+  prop: TKey,
+): <TObj extends Partial<Record<TKey, unknown>>>(obj: TObj) => TObj[TKey];
+export function get<TKey extends string, TDefault>(
+  prop: TKey,
+  defaultValue: TDefault,
+): <TObj extends Partial<Record<TKey, TDefault | undefined>>>(value: TObj) => TDefault;
+export function get<TKey extends string, TDefault>(
+  prop: TKey,
+  defaultValue?: TDefault,
+) {
+  return function <TObj extends Partial<Record<TKey, TDefault | undefined>>>(value: TObj) {
+    const val = value[prop];
+    return val === undefined ? defaultValue : val;
   };
 }
