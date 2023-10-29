fa96cd7e6bc1055ec6547fb36090d0dcce51eafd
William Thorsen <william@thorsen.dev>
2023-10-29 15:25:15 -0700

    arrays|feat: Treat undefined weights array as uniform weights
    Modified `accumulateWeights` to treat an array of entirely undefined weights as a uniform distribution

--- libraries/arrays/src/toCumulativeValues.ts
+++ libraries/arrays/src/toCumulativeValues.ts
@@ -2,34 +2,17 @@
  * Returns an array in which each element contains the running total of the input array up to the same index.
  * Undefined values are treated as 0.
  */
-export function toCumulativeValues(weights: ReadonlyArray<number | undefined>): number[] {
-  const cumulativeWeights: number[] = [];
-  let cumulativeWeight = 0;
+export function toCumulativeValues(values: ReadonlyArray<number | undefined>): number[] {
+  const cumulativeValues: number[] = [];
+  let cumulativeValue = 0;
 
-  for (const weight of weights) {
-    const addend = weight ?? 0;
-    if (addend < 0) {
-      throw new Error('All weights must be non-negative numbers.');
-    }
-
-    cumulativeWeight += addend;
-    cumulativeWeights.push(cumulativeWeight);
+  for (const weight of values) {
+    cumulativeValue += weight ?? 0;
+    cumulativeValues.push(cumulativeValue);
   }
 
-  return cumulativeWeights;
+  return cumulativeValues;
 }
 
 /** @deprecated Use `toCumulativeValues` */
 export const getCumulativeWeights = toCumulativeValues;
-
-/**
- * Wrapper for `toCumulativeValues` allowing it to be used with items that are not weights but have extractable weights.
- * @items The array of items.
- * @getWeight A function that extracts the weight of each item.
- */
-export function accumulateWeights<T>(
-  items: ReadonlyArray<T>,
-  getWeight: (item: T, index: number, items: ReadonlyArray<T>) => number | undefined,
-) {
-  return toCumulativeValues(items.map(getWeight));
-}
